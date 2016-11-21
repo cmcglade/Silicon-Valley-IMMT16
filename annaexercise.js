54bfbfe05723d95fcccb148f3ac3399d61d7ef18
Анна Вериковская <annaverikovska@cr33py.local>
2016-11-21 16:21:33 -0500

    adding my about page and javascript

---
+++ annaexercise.js
@@ -0,0 +1,17 @@
+function addNumbers(num1, num2) {
+	var AdditionResult = num1 + num2;
+	return AdditionResult; 
+}
+function calculateAge(birthyear, currentyear) {
+	var age = currentyear - birthyear; 
+	var result = 'You are either ' + age + ' or ' + (age - 1);
+	return result;
+}
+function js_style() {
+	text.style.fontSize="30px";
+	text.style.fontFamily = "Courier";
+	text.style.color = "#6b94d6";
+	text.style.fontStyle = "bold";
+	text.style.backgroundColor = "#cce6ff";
+
+}
